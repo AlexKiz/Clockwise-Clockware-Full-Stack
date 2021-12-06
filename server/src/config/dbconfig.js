25500acdcffb7b4d2f16b883d2e90637d7cb89e7
Alexey Kiz <alexauvrus@gmail.com>
2021-12-06 10:26:51 +0200

    Added migration

---
+++ server/src/config/dbconfig.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+export const development = {
+  username: process.env.DB_USER_DEV,
+  password: process.env.DB_PASSWORD_DEV,
+  database: process.env.DB_DATABASE_DEV,
+  host: process.env.DB_HOST_DEV,
+  dialect: "postgres"
+}
+export const test = {
+  username: process.env.DB_USER_DEV,
+  password: process.env.DB_PASSWORD_DEV,
+  database: process.env.DB_DATABASE_DEV,
+  host: process.env.DB_HOST_DEV,
+  dialect: "postgres"
+}
+export const production = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}
+
---
+++ server/src/migrations/20211202145435-create-clock.js
@@ -0,0 +1,31 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('clocks', {
+      id: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+    },
+
+    size: {
+        type: Sequelize.DataTypes.STRING(20),
+        allowNull: false
+    },
+
+    price: {
+        type: Sequelize.DataTypes.INTEGER
+    },
+
+    installation_time: {
+        type: Sequelize.DataTypes.INTEGER,
+
+    }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('clocks');
+  }
+};
