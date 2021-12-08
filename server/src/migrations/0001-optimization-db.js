0850d397ec46d921de256829e706f43896a6a443
Alexey Kiz <alexauvrus@gmail.com>
2021-12-08 17:14:12 +0200

    added migration for user table

--- server/src/migrations/0001-optimization-db.js
+++ server/src/migrations/0001-optimization-db.js
@@ -5,7 +5,9 @@ module.exports = {
 		const transaction = await queryInterface.sequelize.transaction();
 		try {
 			await queryInterface.removeColumn('masters', 'ratedSum', {transaction});
+
 			await queryInterface.removeColumn('masters', 'ratedQuantity', {transaction});
+
 			await queryInterface.changeColumn('masters', 'id', {
 				type: Sequelize.DataTypes.UUID,
 				allowNull: false,
@@ -14,6 +16,7 @@ module.exports = {
 				onDelete: 'CASCADE',
 				onUpdate: 'CASCADE',
 			}, {transaction});
+
 			await queryInterface.changeColumn('users', 'id', {
 				type: Sequelize.DataTypes.UUID,
 				allowNull: false,
@@ -22,6 +25,7 @@ module.exports = {
 				onDelete: 'CASCADE',
 				onUpdate: 'CASCADE',
 			}, {transaction});
+
 			await queryInterface.changeColumn('orders', 'id', {
 				type: Sequelize.DataTypes.UUID,
 				allowNull: false,
@@ -29,7 +33,20 @@ module.exports = {
 				defaultValue: UUID4,
 				onDelete: 'CASCADE',
 				onUpdate: 'CASCADE',
-			});
+			}, {transaction});
+
+			await queryInterface.addColumn('users', 'password', {
+				type: Sequelize.DataTypes.STRING(100),
+				allowNull: false,
+			}, {transaction});
+
+			await queryInterface.addColumn('users', 'role', {
+				type: Sequelize.DataTypes.STRING(30),
+				allowNull: false,
+			}, {transaction});
+
+			await queryInterface.dropTable('admin', {transaction});
+
 			await transaction.commit();
 		} catch (err) {
 			await transaction.rollback();
@@ -47,6 +64,7 @@ module.exports = {
 					defaultValue: 0,
 				},
 				{transaction});
+
 			await queryInterface.addColumn('masters', 'ratedQuantity',
 				{
 					type: DataTypes.INTEGER,
@@ -54,6 +72,59 @@ module.exports = {
 					defaultValue: 0,
 				},
 				{transaction});
+
+			await queryInterface.changeColumn('masters', 'id', {
+				type: Sequelize.DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+				autoIncrement: true,
+				onDelete: 'CASCADE',
+				onUpdate: 'CASCADE',
+			}, {transaction});
+
+			await queryInterface.changeColumn('users', 'id', {
+				type: Sequelize.DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+				autoIncrement: true,
+				onDelete: 'CASCADE',
+				onUpdate: 'CASCADE',
+			}, {transaction});
+
+			await queryInterface.changeColumn('orders', 'id', {
+				type: Sequelize.DataTypes.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+				autoIncrement: true,
+				onDelete: 'CASCADE',
+				onUpdate: 'CASCADE',
+			}, {transaction});
+
+			await queryInterface.removeColumn('users', 'password', {transaction});
+
+			await queryInterface.removeColumn('users', 'role', {transaction});
+
+			await queryInterface.createTable('admin', {
+				id: {
+					type: Sequelize.DataTypes.INTEGER,
+					allowNull: false,
+					primaryKey: true,
+					autoIncrement: true,
+					onDelete: 'CASCADE',
+					onUpdate: 'CASCADE',
+				},
+
+				email: {
+					type: Sequelize.DataTypes.STRING,
+					allowNull: false,
+				},
+
+				password: {
+					type: Sequelize.DataTypes.STRING,
+					allowNull: false,
+				},
+			}, {transaction});
+
 			await transaction.commit();
 		} catch (err) {
 			await transaction.rollback();
