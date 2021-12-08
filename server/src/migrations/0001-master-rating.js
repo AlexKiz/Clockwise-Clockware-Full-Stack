335b2e51a9532d49c88095c2fdf54637915ea492
Alexey Kiz <alexauvrus@gmail.com>
2021-12-08 14:24:45 +0200

    Added master rating migration

---
+++ server/src/migrations/0001-master-rating.js
@@ -0,0 +1,37 @@
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		const transaction = await queryInterface.sequelize.transaction();
+		try {
+			await queryInterface.removeColumn('masters', 'ratedSum', {transaction});
+			await queryInterface.removeColumn('masters', 'ratedQuantity', {transaction});
+			await transaction.commit();
+		} catch (err) {
+			await transaction.rollback();
+			throw err;
+		}
+	},
+
+	async down(queryInterface, Sequelize) {
+		const transaction = await queryInterface.sequelize.transaction();
+		try {
+			await queryInterface.addColumn('masters', 'ratedSum',
+				{
+					type: DataTypes.REAL,
+					allowNull: false,
+					defaultValue: 0,
+				},
+				{transaction});
+			await queryInterface.addColumn('masters', 'ratedQuantity',
+				{
+					type: DataTypes.INTEGER,
+					allowNull: false,
+					defaultValue: 0,
+				},
+				{transaction});
+			await transaction.commit();
+		} catch (err) {
+			await transaction.rollback();
+			throw err;
+		}
+	},
+};
