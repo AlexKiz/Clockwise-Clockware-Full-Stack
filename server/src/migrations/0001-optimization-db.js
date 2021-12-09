69d0fd98a3069f640da036013a8c065c35e9caa2
Alexey Kiz <alexauvrus@gmail.com>
2021-12-09 19:03:18 +0200

    Modified migration

--- server/src/migrations/0001-optimization-db.js
+++ server/src/migrations/0001-optimization-db.js
@@ -7,7 +7,6 @@ module.exports = {
 		try {
 			await queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";', {transaction});
 
-			// Generate new UUID for entities
 			await queryInterface.addColumn('users', 'newId', {
 				type: Sequelize.DataTypes.UUID,
 				allowNull: false,
@@ -24,7 +23,6 @@ module.exports = {
 				defaultValue: Sequelize.literal('uuid_generate_v4()'),
 			}, {transaction});
 
-			// Remove all dependent constraints
 			await queryInterface.removeConstraint('orders', 'orders_userId_fkey', {transaction});
 			await queryInterface.removeConstraint('orders', 'orders_masterId_fkey', {transaction});
 			await queryInterface.removeConstraint('master_cities', 'master_cities_masterId_fkey', {transaction});
@@ -33,7 +31,6 @@ module.exports = {
 			await queryInterface.removeConstraint('users', 'users_pkey', {transaction});
 			await queryInterface.removeConstraint('masters', 'masters_pkey', {transaction});
 
-			// Add new column for entities with foreign relationship
 			await queryInterface.addColumn('orders', 'newUserId', {
 				type: Sequelize.DataTypes.UUID,
 			}, {transaction});
@@ -44,18 +41,15 @@ module.exports = {
 				type: Sequelize.DataTypes.UUID,
 			}, {transaction});
 
-			// Add generated uuid's to entities with foreign relationship
 			await queryInterface.sequelize.query('UPDATE orders T SET "newUserId" = (SELECT newId FROM users WHERE id = T.id;', {transaction});
 			await queryInterface.sequelize.query('UPDATE orders T SET "newMasterId" = (SELECT newId FROM masters WHERE id = T.id;', {transaction});
 			await queryInterface.sequelize.query('UPDATE master_cities T SET "newMasterId" = (SELECT newId FROM masters WHERE id = T."masterId";', {transaction});
 
-			// Remove old id's
 			await queryInterface.removeColumn('user', 'id', {transaction});
 			await queryInterface.removeColumn('master', 'id', {transaction});
 			await queryInterface.removeColumn('order', 'id', {transaction});
 			await queryInterface.removeColumn('master_cities', 'masterId', {transaction});
 
-			// Rename all new uuid's to origin names
 			await queryInterface.renameColumn('user', 'newId', 'id', {transaction});
 			await queryInterface.renameColumn('masters', 'newId', 'id', {transaction});
 			await queryInterface.renameColumn('orders', 'newId', 'id', {transaction});
@@ -63,7 +57,6 @@ module.exports = {
 			await queryInterface.renameColumn('orders', 'newMasterId', 'masterId', {transaction});
 			await queryInterface.renameColumn('orders', 'newUserId', 'userId', {transaction});
 
-			// Add allowNull: false
 			await queryInterface.changeColumn('orders', 'masterId', {
 				allowNull: false,
 				onDelete: 'CASCADE',
@@ -82,12 +75,10 @@ module.exports = {
 				onUpdate: 'CASCADE',
 			}, {transaction});
 
-			// Create Primary relationships to all changed entities
 			await queryInterface.sequelize.query('ALTER TABLE IF EXISTS public.users ADD CONSTRAINT users_pkey PRIMARY KEY (id)', {transaction});
 			await queryInterface.sequelize.query('ALTER TABLE IF EXISTS public.orders ADD CONSTRAINT orders_pkey PRIMARY KEY (id)', {transaction});
 			await queryInterface.sequelize.query('ALTER TABLE IF EXISTS public.masters ADD CONSTRAINT masters_pkey PRIMARY KEY (id)', {transaction});
 
-			// Create Foreign relationships with all entities
 			await queryInterface.sequelize.query(`ALTER TABLE IF EXISTS public.orders
 												ADD CONSTRAINT "orders_userId_fkey" FOREIGN KEY ("userId")
 												REFERENCES public.users (id) MATCH SIMPLE
@@ -114,6 +105,23 @@ module.exports = {
 
 			await queryInterface.sequelize.query(`ALTER TABLE IF EXISTS public.master_cities
 												ADD CONSTRAINT master_cities_pkey PRIMARY KEY ("masterId", "cityId");`, {transaction});
+
+
+			await queryInterface.addColumn('users', 'password', {
+				type: Sequelize.DataTypes.STRING(100),
+				allowNull: false,
+			}, {transaction});
+
+			await queryInterface.addColumn('users', 'role', {
+				type: Sequelize.DataTypes.STRING(30),
+				allowNull: false,
+			}, {transaction});
+
+			await queryInterface.dropTable('admin', {transaction});
+
+			await queryInterface.removeColumn('masters', 'ratedSum', {transaction});
+
+			await queryInterface.removeColumn('masters', 'ratedQuantity', {transaction});
 
 			await transaction.commit();
 		} catch (err) {
