9794e344224268fbcc6507e059a91208f1841b14
Alexey Kiz <alexauvrus@gmail.com>
2021-12-22 18:00:48 +0200

    Completed master registration

---
+++ client/src/components/Admin/users/verification/Verification.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-mixed-spaces-and-tabs */
+import axios from 'axios';
+import React, {useEffect, FC} from 'react';
+import {useHistory, useParams} from 'react-router-dom';
+import classes from '../verification/verification.module.css';
+import {URL} from '../../../../data/constants/routeConstants';
+import {Params} from 'src/data/types/types';
+import {Button, Stack} from '@mui/material';
+import {VerifiactionProps} from './componentConstants';
+
+
+const Verification: FC<VerifiactionProps> = () => {
+	const history = useHistory();
+
+	const {hashVerify} = useParams<Params>();
+
+	useEffect(() => {
+		const verifyUser = async () => {
+			const user = await axios.put(URL.VERIFY, {
+				hashVerify,
+			});
+			if (!user[0]) {
+				history.push(URL.LOGIN);
+			}
+		};
+		verifyUser();
+	}, []);
+
+
+	const handleOnClick = () => {
+		history.push(URL.LOGIN);
+	};
+
+	return (
+		<div>
+
+			<div className={classes.container}>
+				<div className={classes.container_notification}>
+					<Stack direction='column' justifyContent='center' spacing={2}>
+						<div className={classes.form_label}>
+							<label>CONGRATULATIONS! You have verified your email!</label>
+						</div>
+
+						<div className={classes.form_section}>
+							<Button
+								variant="contained"
+								onClick={handleOnClick}
+								className={classes.btn}
+								style={ {fontSize: 22, backgroundColor: 'green', borderRadius: 15} }
+							>
+						GO TO MAIN PAGE
+							</Button>
+						</div>
+					</Stack>
+				</div>
+			</div>
+
+		</div>
+	);
+};
+
+export default Verification;
---
+++ client/src/components/Admin/users/verification/componentConstants.ts
@@ -0,0 +1,1 @@
+export interface VerifiactionProps {};
