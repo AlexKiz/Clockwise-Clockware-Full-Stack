776970cb9adfe8531e4a9c1451933a1b6ac775e6
Alexey Kiz <alexauvrus@gmail.com>
2021-12-29 14:33:05 +0200

    Added pagination to order list

--- client/src/components/Admin/orders/list/OrdersList.tsx
+++ client/src/components/Admin/orders/list/OrdersList.tsx
@@ -668,7 +668,7 @@ const OrdersList: FC<OrdersListProps> = () => {
 							<TableRow>
 								<TablePagination
 									rowsPerPageOptions={[5, 10, 25, {label: 'All', value: totalQuantity}]}
-									colSpan={10}
+									colSpan={9}
 									count={totalQuantity}
 									rowsPerPage={limit}
 									page={page}
