996c3c25e88bcaef716318c1beeeb6fcce43cad0
AlexKiz <alexauvrus@gmail.com>
2022-01-23 12:20:58 +0200

    added cloudinary class

--- server/src/services/cloudinary.ts
+++ server/src/services/cloudinary.ts
@@ -1,13 +1,33 @@
 /* eslint-disable require-jsdoc */
-require('dotenv').config();
+import dotenv from 'dotenv';
 import * as cloudinary from 'cloudinary';
+import {resolve} from 'path/posix';
 
-cloudinary.v2.config({
-	cloud_name: process.env.CLOUD_NAME,
-	api_key: process.env.CLOUD_API_KEY,
-	api_secret: process.env.CLOUD_API_SECRET,
-});
+dotenv.config();
 
-export {cloudinary};
+interface IConfig {
+	cloud_name: string;
+	api_key: string;
+	api_secret: string;
+}
 
+interface IData {
+	url: string
+}
 
+export class CloudinaryService {
+	constructor(config: IConfig) {
+		cloudinary.v2.config(config);
+	}
+
+	public uploadPhotos = (photos: string[]) => {
+		return Promise.all(
+			photos.map((photo) => {
+				return cloudinary.v2.uploader.upload(photo)
+					.then((data: IData) => {
+						resolve(data.url);
+					});
+			}),
+		);
+	};
+}
